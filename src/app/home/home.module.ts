e6a435bb370117cf9b77f7a1a75a2493c2a3714b
Bala1324 <balachandiran1324@gmail.com>
2022-07-23 11:43:39 +0530

    ionic basic

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {ButtonComponent} from '../components/button/button.component'
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,ButtonComponent]
 })
 export class HomePageModule {}
